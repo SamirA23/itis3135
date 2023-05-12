3d7acb176e5bb0256962050bc9e0a1cd13500d6e
SamirA23 <samir.appikatla@gmail.com>
2023-05-12 10:08:48 -0400

    adding files

---
+++ private_html/itis3135/itis3135/scripts/polygons.js
@@ -0,0 +1,15 @@
+let text;
+let favDrink = prompt("The Scrumptious Sloth would like your number...");
+switch(favDrink) {
+  case "Coca-Cola":
+    text = "Excellent choice! Coca-Cola is good for your soul.";
+    break;
+  case "Pepsi":
+    text = "Pepsi is my favorite too!";
+    break;
+  case "Sprite":
+    text = "Really? Are you sure the Sprite is your favorite?";
+    break;
+  default:
+    text = "I have never heard of that one!";
+}
